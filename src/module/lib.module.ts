11184220efc0793af5f7dac543784c103c336ee7
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2017-11-23 20:46:32 +0530

    feat(component): AddedComponents

--- src/module/lib.module.ts
+++ src/module/lib.module.ts
@@ -6,6 +6,8 @@ import { LibService } from './service/lib.service';
 import {AmxTextComponent} from './component/forms/textinput/textinput.component';
 import {FormsModule} from "@angular/forms";
 import {CheckBoxComponent} from "./component/forms/checkbox/checkbox.component";
+import {TypeAheadComponent} from "./component/forms/typeahead/typeahead.component";
+import {DropDownComponent} from "./component/forms/dropdown/dropdown.component";
 
 // Export module's public API
 export { LibComponent } from './component/lib.component';
@@ -16,8 +18,8 @@ export { LibService } from './service/lib.service';
     CommonModule,
     FormsModule
   ],
-  exports: [LibComponent, AmxTextComponent,CheckBoxComponent],
-  declarations: [LibComponent, AmxTextComponent,CheckBoxComponent]
+  exports: [LibComponent, AmxTextComponent,TypeAheadComponent,DropDownComponent],
+  declarations: [LibComponent, AmxTextComponent,TypeAheadComponent,DropDownComponent]
 })
 export class LibModule {
   static forRoot(): ModuleWithProviders {
